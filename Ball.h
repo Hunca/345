ea84d033ab8a07a7ba72d3f50530ea4edaff0f89
chali172 <liam.nicholas.chapman@gmail.com>
2019-05-16 14:26:37 +1200

    Small changes to Ball datafields

--- Ball.h
+++ Ball.h
@@ -3,9 +3,10 @@
 class Ball {
 private:
 	int num;
-	float x, y, radius, speed;
+	float x, y, vx, vy, ax, ay, radius, speed;
+	bool isSunk, isMoving;
 public:
-	Ball(int ballNum, float xPos, float yPos, float rad);
+	Ball(int ballNum, float rad, float xPos, float yPos);
 	~Ball();
 	float getX();
 	float getY();
