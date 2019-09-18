5fde78217a33f1a4f6649db1fef7c6e72b3ccbfa
chali172 <liam.nicholas.chapman@gmail.com>
2019-09-18 19:08:48 +1200

    Balls now seem to bounce off the walls in a realistic way. If any lines were to be added in the balls would bounce off them normally.

--- Physics.cpp
+++ Physics.cpp
@@ -26,31 +26,27 @@ void Physics::ballCollision(Ball *ballA, Ball *ballB) {
     //ballPenetrationHandler
     float fDistance = sqrtf((ballA->x - ballB->x) * (ballA->x - ballB->x) + (ballA->y - ballB->y) * (ballA->y - ballB->y));
     float distanceToMove = fDistance - ballA->radius - ballB->radius;
-    ballA->x -= (distanceToMove*0.5f) * (ballA->x - ballB->x) / fDistance;
-    ballA->y -= (distanceToMove*0.5f) * (ballA->y - ballB->y) / fDistance;
-    ballB->x += (distanceToMove*0.5f) * (ballA->x - ballB->x) / fDistance;
-    ballB->y += (distanceToMove*0.5f) * (ballA->y - ballB->y) / fDistance;
-    
+    if(ballB->num > 0){
+        ballA->x -= (distanceToMove*0.5f) * (ballA->x - ballB->x) / fDistance;
+        ballA->y -= (distanceToMove*0.5f) * (ballA->y - ballB->y) / fDistance;
+        ballB->x += (distanceToMove*0.5f) * (ballA->x - ballB->x) / fDistance;
+        ballB->y += (distanceToMove*0.5f) * (ballA->y - ballB->y) / fDistance;
+    }
     // Normal
     float nx = (ballB->x - ballA->x) / fDistance;
     float ny = (ballB->y - ballA->y) / fDistance;
-
     // Tangent
     float tx = -ny;
     float ty = nx;
-
     // Dot Product Tangent
     float dpTan1 = ballA->vx * tx + ballA->vy * ty;
     float dpTan2 = ballB->vx * tx + ballB->vy * ty;
-
     // Dot Product Normal
     float dpNorm1 = ballA->vx * nx + ballA->vy * ny;
     float dpNorm2 = ballB->vx * nx + ballB->vy * ny;
-
     // Conservation of momentum in 1D
     float m1 = (dpNorm1 * (ballA->mass - ballB->mass) + 2.0f * ballB->mass * dpNorm2) / (ballA->mass + ballB->mass);
     float m2 = (dpNorm2 * (ballB->mass - ballA->mass) + 2.0f * ballA->mass * dpNorm1) / (ballA->mass + ballB->mass);
-
     // Update ball velocities
     ballA->vx = tx * dpTan1 + nx * m1;
     ballA->vy = ty * dpTan1 + ny * m1;
@@ -60,42 +56,13 @@ void Physics::ballCollision(Ball *ballA, Ball *ballB) {
 
 void Physics::cushionCollision(Ball *ballA, float x, float y) {
     Ball *ballB = new Ball(-1, 8.55f, x, y);
-    ballB->mass = ballA->mass * 8.f;
+    ballB->mass = ballA->mass * 0.8f;
     ballB->vx = -ballA->vx;
     ballB->vy = -ballA->vy;
     ballB->x-=ballB->radius;
     ballB->y-=ballB->radius;
 
-    float fDistance = sqrtf((ballA->x - ballB->x) * (ballA->x - ballB->x) + (ballA->y - ballB->y) * (ballA->y - ballB->y));
-    float distanceToMove = fDistance - ballA->radius - ballB->radius;
-    ballA->x -= (distanceToMove*0.5f) * (ballA->x - ballB->x) / fDistance;
-    ballA->y -= (distanceToMove*0.5f) * (ballA->y - ballB->y) / fDistance;
-    ballB->x += (distanceToMove*0.5f) * (ballA->x - ballB->x) / fDistance;
-    ballB->y += (distanceToMove*0.5f) * (ballA->y - ballB->y) / fDistance;
-    
-    // Normal
-    float nx = (ballB->x - ballA->x) / fDistance;
-    float ny = (ballB->y - ballA->y) / fDistance;
-
-    // Tangent
-    float tx = -ny;
-    float ty = nx;
-
-    // Dot Product Tangent
-    float dpTan1 = ballA->vx * tx + ballA->vy * ty;
-    float dpTan2 = ballB->vx * tx + ballB->vy * ty;
-
-    // Dot Product Normal
-    float dpNorm1 = ballA->vx * nx + ballA->vy * ny;
-    float dpNorm2 = ballB->vx * nx + ballB->vy * ny;
-
-    // Conservation of momentum in 1D
-    float m1 = (dpNorm1 * (ballA->mass - ballB->mass) + 2.0f * ballB->mass * dpNorm2) / (ballA->mass + ballB->mass);
-    float m2 = (dpNorm2 * (ballB->mass - ballA->mass) + 2.0f * ballA->mass * dpNorm1) / (ballA->mass + ballB->mass);
-
-    // Update ball velocities
-    ballA->vx = tx * dpTan1 + nx * m1;
-    ballA->vy = ty * dpTan1 + ny * m1;
+    ballCollision(ballA, ballB);
 }
 bool Physics::overLapDetection(Ball *whiteBall, Ball *ball) {
     float x1 = whiteBall->x + whiteBall->radius, x2 = ball->x + ball->radius;
