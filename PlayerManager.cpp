821e392e3072efe48af92bd50ffd55a4c3a5a5a7
MSI\calum <calumhunter256@gmail.com>
2019-09-21 17:35:24 +1200

    got rid of the left and right aimers

--- PlayerManager.cpp
+++ PlayerManager.cpp
@@ -15,40 +15,8 @@ void PlayerManager::setPower(Ball *whiteBall, bool elevation) {
         poolCue.setPosition(poolCue.getPosition().x + xChange, poolCue.getPosition().y + yChange);
     }
 }
-void PlayerManager::left(Ball *whiteBall) {
-    float speed = 2; //speed of rotation
-    float x1 = poolCue.getPosition().x - (whiteBall->x + (whiteBall->radius / 2));
-    float y1 = poolCue.getPosition().y - (whiteBall->y + (whiteBall->radius / 2));
-    float x2 = x1 * cos(-speed * dt) - y1 * sin(-speed * dt);
-    float y2 = x1 * sin(-speed * dt) + y1 * cos(-speed * dt);
-
-    poolCue.setPosition(x2 + (whiteBall->x + (whiteBall->radius / 2)), y2 + (whiteBall->y + (whiteBall->radius / 2)));
-
-    line[0] = sf::Vertex(sf::Vector2f(poolCue.getPosition().x + poolCue.getRadius(), poolCue.getPosition().y + poolCue.getRadius()), sf::Color::Black);
-    line[1] = sf::Vertex(sf::Vector2f(whiteBall->x + whiteBall->radius, whiteBall->y + whiteBall->radius), sf::Color::Black);
-}
-
-void PlayerManager::right(Ball *whiteBall) {
-    float angle = 2; //speed of rotation
-    float x1 = poolCue.getPosition().x - (whiteBall->x + (whiteBall->radius / 2));
-    float y1 = poolCue.getPosition().y - (whiteBall->y + (whiteBall->radius / 2));
-
-    float x2 = x1 * cos(angle * dt) - y1 * sin(angle * dt);
-    float y2 = x1 * sin(angle * dt) + y1 * cos(angle * dt);
-
-    poolCue.setPosition(x2 + (whiteBall->x + (whiteBall->radius / 2)), y2 + (whiteBall->y + (whiteBall->radius / 2)));
-
-    line[0] = sf::Vertex(sf::Vector2f(poolCue.getPosition().x + poolCue.getRadius(), poolCue.getPosition().y + poolCue.getRadius()), sf::Color::Black);
-    line[1] = sf::Vertex(sf::Vector2f(whiteBall->x + whiteBall->radius, whiteBall->y + whiteBall->radius), sf::Color::Black);
-}
-
 void PlayerManager::mouseAim(Ball *whiteBall, sf::Event event) {
-    float distance = sqrtf(((event.mouseMove.x - poolCue.getRadius()) - (whiteBall->x + whiteBall->radius)) *
-        ((event.mouseMove.x - poolCue.getRadius()) - (whiteBall->x + whiteBall->radius)) +
-        ((event.mouseMove.y - poolCue.getRadius()) - (whiteBall->y + whiteBall->radius)) * 
-        ((event.mouseMove.y - poolCue.getRadius()) - (whiteBall->y + whiteBall->radius)));
-    
-    if (event.type == sf::Event::MouseMoved){
+    if(event.type == sf::Event::MouseMoved){
         float R = whiteBall->radius + 10;
         float vX = (event.mouseMove.x - poolCue.getRadius()) - (whiteBall->x);
         float vY = (event.mouseMove.y - poolCue.getRadius()) - (whiteBall->y);
@@ -56,10 +24,8 @@ void PlayerManager::mouseAim(Ball *whiteBall, sf::Event event) {
         float aX = (whiteBall->x) + vX / magV * R;
         float aY = (whiteBall->y) + vY / magV * R;
         poolCue.setPosition(aX, aY);
-        
     }
 }
-
 void PlayerManager::playerTurn(Ball *whiteBall, sf::Event event) {
     if(screenSelected == false) return;
     dt = dtClock.restart().asSeconds();
@@ -67,14 +33,8 @@ void PlayerManager::playerTurn(Ball *whiteBall, sf::Event event) {
         ((poolCue.getPosition().x + poolCue.getRadius()) - (whiteBall->x + whiteBall->radius)) +
         ((poolCue.getPosition().y + poolCue.getRadius()) - (whiteBall->y + whiteBall->radius)) * 
         ((poolCue.getPosition().y + poolCue.getRadius()) - (whiteBall->y + whiteBall->radius)));
-    
-    mouseAim(whiteBall, event);
-
-    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right)) {
-        right(whiteBall);
-    }
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left)) {
-        left(whiteBall);
+    if (sf::Mouse::isButtonPressed(sf::Mouse::Button::Left)) {
+        mouseAim(whiteBall, event);
     }
     if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up)) {
         if (distance <= 200) { //alter distance of cue
